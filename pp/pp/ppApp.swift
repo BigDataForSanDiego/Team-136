c9d229806d45e2b0e7afc604d0c9da3c1aaab19c
matttwh0 <matttran2004@gmail.com>
2024-10-12 15:08:13 -0700

    hi

---
+++ pp/pp/ppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ppApp.swift
+//  pp
+//
+//  Created by Matthew Tran on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
