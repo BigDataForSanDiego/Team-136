ab4315508c14c34acd6dea6400cd8d21d6a7239c
VictorTepordei <victor.tepordei@outlook.com>
2024-10-15 21:42:44 -0700

    make button switch from red to black

--- pp/pp/Views/AddMedication.swift
+++ pp/pp/Views/AddMedication.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct AddMedication: View {
-    var body: some View {
+    
         //Textfield
         @State var textFieldText: String = ""
-        let color = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        @State var buttonColor: Color = Color.blue
+        var body: some View {
+            let color = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         //prompt user to input information
         
             //TODO: put the button next to the text field and add this file to ListView
@@ -25,9 +27,12 @@ struct AddMedication: View {
                     .cornerRadius(10)
                     
                 
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Button(action: {
+                    buttonColor = (buttonColor == Color.blue) ? Color.red : Color.blue
+                }, label: {
                     Label("", systemImage: "arrowshape.up.circle.fill")
                         .cornerRadius(10)
+                        .foregroundColor(buttonColor)
                         .font(.largeTitle)
                         .dynamicTypeSize(.xxxLarge)
                 })
