93de7e5a27432acab29182dc982f2794e8a90cec
matttwh0 <matttran2004@gmail.com>
2024-10-16 15:26:55 -0700

    add api key + url,

--- pp/pp/Views/ViewController.swift
+++ pp/pp/Views/ViewController.swift
@@ -1,7 +1,20 @@
 //
 //  ViewController.swift
 //  pp
-//
+//https://api.fda.gov/drug/event.json?api_key=yourAPIKeyHere&search=...
 //  Created by Victor Tepordei on 10/16/24.
-//
+//API KEY: 56pDfckELT0JUR0MgZaPwPVs8MKieO3Gs3NkeYgM
+import UIKit
+import Foundation
 
+class ViewController: UIViewController{
+    
+    //get all the data we need
+    func getDrugData() {
+
+
+        let apiURL = "https://api.fda.gov/drug/label.json"
+        let apiKey = "https://api.fda.gov/drug/event.json?api_key=56pDfckELT0JUR0MgZaPwPVs8MKieO3Gs3NkeYgM&search=..."
+        
+    }
+}
