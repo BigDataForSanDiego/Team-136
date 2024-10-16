17110e456b08911187a8a4659838e67e1670ce11
matttwh0 <matttran2004@gmail.com>
2024-10-15 20:09:35 -0700

    UI changes to ListView for improved simplicity

--- pp/pp/Views/Listview.swift
+++ pp/pp/Views/Listview.swift
@@ -6,10 +6,9 @@
 //
 
 import SwiftUI
-
+    
 struct ListView: View {
     var body: some View {
-        
         
         //store sentence values of todo list
         @State var items: [String] = [
@@ -17,27 +16,25 @@ struct ListView: View {
             "Medicine added 2",
             "testing new branch"
         ]
-        
-        
-        List {
-            //iterate through items array
-            ForEach(items, id: \.self){ item in
-                ListRowView(title: item)
-            }
-            .listStyle(PlainListStyle())
+        VStack{
             
-
+            //call to addmedication file
+            AddMedication()
+            
+            List {
+                
+                //iterate through items array
+                ForEach(items, id: \.self){ item in
+                    ListRowView(title: item)
+                }
+                .listStyle(PlainListStyle())
+                
+                }
+            //title
+            .navigationTitle("Prescription Pals 💊")
+            }
         }
-        //title
-        .navigationTitle("Prescription Pals 💊")
-        .navigationBarItems(
-            leading: EditButton(),
-            trailing:
-                NavigationLink("Add", destination: AddMedication())
-        )
-       
     }
-}
 
 struct ListView_Previews: PreviewProvider{
     static var previews: some View {
